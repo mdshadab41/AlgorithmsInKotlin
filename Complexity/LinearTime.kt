2d08d881d67506258148edcd9d3a11c520d9be9b
Shadab <shadabjhu00447@gmail.com>
2023-08-09 16:23:37 +0530

    Complexity

---
+++ Complexity/LinearTime.kt
@@ -0,0 +1,13 @@
+package Complexity
+
+fun printNumbers(numbers: List<Int>) {
+    for (number in numbers) {
+        println(number)
+    }
+}
+
+fun main(){
+
+    printNumbers(listOf(1, 2, 3, 4, 5, 6))
+
+}
